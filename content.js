a9e88798cf59771ad6cf919c59660faab43b4278
Michelle Domagala-Tang <45515385+domagalm@users.noreply.github.com>
2020-01-11 18:53:20 -0500

    Updated comment identification

--- content.js
+++ content.js
@@ -1,1 +1,18 @@
-consule.log("Chrome Extension");
+setTimeout(function() {
+
+if (window.location.href.indexOf("watch") > -1) {
+    var i = 0;
+
+    while (document.querySelectorAll("yt-formatted-string#content-text")[i].textContent){
+        let data = document.querySelectorAll("yt-formatted-string#content-text")[i].textContent;
+        console.log(data);
+
+        if (data) {
+            document.querySelectorAll("yt-formatted-string#content-text")[i].style.color = "white";
+        }
+        i++;
+    }
+}
+
+},5000);
+
