eca438e224b1df3954eba72579d83c79e5da8afc
Michelle Domagala-Tang <45515385+domagalm@users.noreply.github.com>
2020-01-11 19:11:08 -0500

    Updated content.js

--- content.js
+++ content.js
@@ -7,9 +7,26 @@ if (window.location.href.indexOf("watch") > -1) {
         let data = document.querySelectorAll("yt-formatted-string#content-text")[i].textContent;
         console.log(data);
 
+        let fetchData = {
+            method: 'POST',
+            body: data,
+            headers: new Headers()
+        }
+
+        const url = 'http://localhost:3000/api/add/comment';
+
+        fetch(url, fetchData) // Call the fetch function passing the url of the API as a parameter
+        .then(function() {
+            // Your code for handling the data you get from the API
+        })
+        .catch(function() {
+            // This is where you run code if the server returns any errors
+        });
+
         if (data) {
             document.querySelectorAll("yt-formatted-string#content-text")[i].style.color = "white";
         }
+
         i++;
     }
 }
